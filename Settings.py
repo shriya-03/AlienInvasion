1e93b1f0ee29aecf6e7c5c4a3e9f415ca1fd57c0
Shriya S <103843531+shriya-03@users.noreply.github.com>
2025-08-03 17:34:57 +0530

    Add files via upload

---
+++ Settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (135, 206, 235)  
+
+        
+        self.ship_speed = 1.5 #How fast the ship moves in pixels per frame.
+
+        
