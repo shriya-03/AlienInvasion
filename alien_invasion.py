a1896b3d594cbb1aed71638e5513cf21f9f6ec91
Shriya S <103843531+shriya-03@users.noreply.github.com>
2025-08-04 17:43:15 +0530

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from Settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -13,12 +14,15 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         #Creates the game window using the width/height from the Settings object.
         pygame.display.set_caption("Alien Invasion")
+        
 
         # Set the background color.
         self.bg_color = self.settings.bg_color
 
         # Create a ship.
         self.ship = Ship(self)
+
+        self.bullet = None  # No bullet on screen at start
 # Creates the player’s ship.
 # Passes the self object (the whole game) so the ship can access the screen and settings.
 
@@ -35,17 +39,36 @@ class AlienInvasion:
                     elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = True
 
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                           if self.bullet is None:  # Only fire if no bullet
+                               self.bullet = Bullet(self)
+
                 elif event.type == pygame.KEYUP:#If the player released a key
                     if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
                     elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = False
 
+                
+
+
             self.ship.update()
             #Calls the update() method from ship.py.
 #This checks the flags moving_right and moving_left, and moves the ship if needed.
+            # Update bullet
+            if self.bullet:
+    # Only update position if bullet is not at top
+               if self.bullet.rect.top > 0:
+                   self.bullet.update()
+
+
+
+
 
             self.screen.fill(self.bg_color)
+            if self.bullet:
+                self.bullet.draw()
             self.ship.blitme()
             pygame.display.flip()
 # painting a picture behind a curtain.
