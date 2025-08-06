011068695e4bc7e75ac364f30db0cc7fb2151fb6
Shriya S <103843531+shriya-03@users.noreply.github.com>
2025-08-06 19:45:01 +0530

    Add files via upload

--- Alien.py
+++ Alien.py
@@ -4,6 +4,7 @@ class Alien:
         self.screen = alien_invasion_object.screen #Stores screen and settings
         self.settings = alien_invasion_object.settings
         self.screen_rect = alien_invasion_object.screen.get_rect()
+        
 
         self.image = pygame.image.load('alien.png')
         self.image = pygame.transform.scale(self.image, (60, 48))
@@ -12,6 +13,7 @@ class Alien:
         self.rect.midtop = self.screen_rect.midtop
 
    
-   
+    def blitme(self):#Draws the alien at its current position on the screen.
+        self.screen.blit(self.image, self.rect)
 
      
