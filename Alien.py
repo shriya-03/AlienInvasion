fcf20dff43b6079ac900dec00f809d77fe120824
Shriya S <103843531+shriya-03@users.noreply.github.com>
2025-08-06 17:51:43 +0530

    Add files via upload

---
+++ Alien.py
@@ -0,0 +1,17 @@
+import pygame
+class Alien:
+    def __init__(self,alien_invasion_object):
+        self.screen = alien_invasion_object.screen #Stores screen and settings
+        self.settings = alien_invasion_object.settings
+        self.screen_rect = alien_invasion_object.screen.get_rect()
+
+        self.image = pygame.image.load('alien.png')
+        self.image = pygame.transform.scale(self.image, (60, 48))
+        self.rect = self.image.get_rect()
+
+        self.rect.midtop = self.screen_rect.midtop
+
+   
+   
+
+     
